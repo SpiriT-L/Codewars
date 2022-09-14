aabca66d78054422e8305379f3e4510cff863a39
LeonID <lmatskevich79@gmail.com>
2022-09-15 01:45:32 +0300

    feat: add Human_readable_duration_format.js

--- 4kyu/Human_readable_duration_format.js
+++ 4kyu/Human_readable_duration_format.js
@@ -1,8 +1,50 @@
 function formatDuration(seconds) {
-  // let now = 0;
-  if ((seconds = 1)) {
-    return now;
+  if (seconds <= 0) {
+    return 'now';
   }
+  let minute = 0;
+  let hour = 0;
+  let day = 0;
+  let year = 0;
+
+  const arr = [];
+  const constructorPlural = (i, j) => `${i} ${i > 1 ? j + 's' : j}`;
+
+  if (seconds >= 3600) {
+    hour = Math.floor(seconds / 3600);
+    seconds = seconds - hour * 3600;
+  }
+  if (hour >= 24) {
+    day = Math.floor(hour / 24);
+    hour = hour - day * 24;
+  }
+  if (day >= 365) {
+    year = Math.floor(day / 365);
+    day = day - year * 365;
+  }
+  if (seconds >= 60) {
+    minute = Math.floor(seconds / 60);
+    seconds = seconds - minute * 60;
+  }
+
+  if (year) arr.push(constructorPlural(year, 'year'));
+  if (day) arr.push(constructorPlural(day, 'day'));
+  if (hour) arr.push(constructorPlural(hour, 'hour'));
+  if (minute) arr.push(constructorPlural(minute, 'minute'));
+  if (seconds) arr.push(constructorPlural(seconds, 'second'));
+
+  if (arr.length > 1) {
+    const duration = arr.pop();
+    return arr.join(', ') + ' and ' + duration;
+  }
+
+  return arr[0];
 }
 
+// console.log(formatDuration(0));
 console.log(formatDuration(1));
+console.log(formatDuration(62));
+console.log(formatDuration(120));
+console.log(formatDuration(3700));
+console.log(formatDuration(3600));
+console.log(formatDuration(3662));
